a8c2aeaa744314b2ddc04f19d1359d11fca90529
mcheraghi <msn.cheraghi@gmail.com>
2021-07-27 11:53:34 +0200

    linear_regression is changed

--- linear_Regression.py
+++ linear_Regression.py
@@ -18,6 +18,23 @@ print(tf.__version__)
 
 
 
+
+# ----Simple example
+
+x = [1, 2, 2.5, 3, 4]
+y = [1, 4, 7, 9, 15]
+plt.plot(x, y, 'ro')
+plt.axis([0, 6, 0, 20])
+
+
+plt.plot(x, y, 'ro')
+plt.axis([0, 6, 0, 20])
+plt.plot(np.unique(x), np.poly1d(np.polyfit(x, y, 1))(np.unique(x)))
+plt.show()
+
+
+
+
 # -----Get the data from the archive
 url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data'
 column_names = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight',
